ee642dfa88bc6b4c5e920346fc46bc926a5d60f4
Desintila <desintila.luzi@fshnstudent.info>
2021-12-03 18:55:17 +0100

    added sort

--- index.js
+++ index.js
@@ -5,75 +5,87 @@ const state = {
             name: 'beetroot',
             price: 0.35,
             img: 'assets/icons/001-beetroot.svg',
-            productInCart: 0
+            productInCart: 0,
+            productType: 'vegetable'
         },
         {
             id: 2,
             name: 'carrot',
             price: 0.65,
             img: 'assets/icons/002-carrot.svg',
-            productInCart: 0
+            productInCart: 0,
+            productType: 'vegetable'
         },
         {
             id: 3,
             name: 'apple',
             price: 0.55,
             img: 'assets/icons/003-apple.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'fruit'
         },
         {
             id: 4,
             name: 'apricot',
             price: 2.05,
             img: 'assets/icons/004-apricot.svg',
-            productInCart: 10
+            productInCart: 10,
+            productType: 'fruit'
         },
         {
             id: 5,
             name: 'avocado',
             price: 2.35,
             img: 'assets/icons/005-avocado.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'vegetable'
         },
         {
             id: 6,
             name: 'bananas',
             price: 0.45,
             img: 'assets/icons/006-bananas.svg',
-            productInCart: 6
+            productInCart: 6,
+            productType: 'fruit'
         },
         {
             id: 7,
             name: 'bell-pepper',
             price: 0.64,
             img: 'assets/icons/007-bell-pepper.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'vegetable'
         },
         {
             id: 8,
             name: 'berry',
             price: 1.25,
             img: 'assets/icons/008-berry.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'fruit'
         },
         {
             id: 9,
             name: 'blueberry',
             price: 1.35,
             img: 'assets/icons/009-blueberry.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'fruit'
         },
         {
             id: 10,
             name: 'eggplant',
             price: 0.75,
             img: 'assets/icons/010-eggplant.svg',
-            productInCart: 5
+            productInCart: 5,
+            productType: 'vegetable'
         }
     ]
 }
 console.log(state)
 function createProductCarts() {
+    const productList = document.querySelector('.item-list.store--item-list')
+    productList.innerHTML = ''
     for (const product of state.products) {
         const liEl = document.createElement('li')
         const divEl = document.createElement('div')
@@ -85,17 +97,19 @@ function createProductCarts() {
         buttonEl.textContent = 'Add to cart'
         divEl.append(imgEl)
         liEl.append(divEl, buttonEl)
-        const productList = document.querySelector('.item-list.store--item-list')
+
         productList.append(liEl)
         buttonEl.addEventListener('click', function () {
-
             product.productInCart++
             render()
 
         })
+
+
     }
 }
-createProductCarts()
+
+
 function addNewItem(product) {
     const liEl = document.createElement('li')
     const imgEl = document.createElement('img')
@@ -146,9 +160,35 @@ function renderTotal() {
     const totalEl = document.querySelector(".total-number")
     totalEl.textContent = `£${getTotal().toFixed(2)}`
 }
+function filterFruitProducts() {
+    return state.products.filter(function (product) {
+        return product.productType === 'fruit'
+    })
+
+}
+function filterVegetableProducts() {
+    return state.products.filter(function (product) {
+        return product.productType === 'vegetable'
+    })
+
+}
+function sortProducts() {
+    return state.products.sort(function (a, b) {
+        return a.price - b.price
+    })
+}
+function listenTosort() {
+    const sortProduct = document.querySelector('.sortButton')
+    sortProduct.addEventListener('click', function () {
+        sortProducts()
+        render()
+    })
+}
+listenTosort()
 function render() {
     renderCart()
     renderTotal()
+    createProductCarts()
+
 }
 render()
-
